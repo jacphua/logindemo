91b7ef82f9198ca67558a09f4e8ed94f987c95e1
Jac Phua <jac@witgritfit.com>
2022-08-15 07:14:43 +0300

    Initial Commit from VScode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { FronteggProvider } from "@frontegg/react";
+const contextOptions = {
+  baseUrl: "https://app-anxv0uqz9qjh.frontegg.com",
+  clientId: "7089eeaf-b8a3-461a-9826-caec9f1a396d",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+  <App />
+  </FronteggProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want to start measuring performance in your app, pass a function // to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals reportWebVitals();
